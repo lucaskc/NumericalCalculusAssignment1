0aaef01240306fc4e338f3c834d62e1b9cf4a9cc
Giovane Cunha Mocellin <giovane.mocellin@gmail.com>
2017-08-22 23:13:49 -0300

    Metodo da secante mais fix nos outros 2 metodos

--- bisseccao.py
+++ bisseccao.py
@@ -4,27 +4,30 @@ def f(x):
 	return 42*pow(x,4) - 23*pow(x,3) + 163*pow(x,2) - 92*x - 20
 
 def bisection(a, b, tol, maxiter):
+	
 	if f(a) == 0:
-		return a
+		retString += '0\t' + '%.10f'%(a) + '\t' + '%.10f'%(0) + '\t' + '%.10f'%(0) + '\n'
+		return retString
 	elif f(b) == 0:
-		return b
+		retString += '0\t' + '%.10f'%(b) + '\t' + '%.10f'%(0) + '\t' + '%.10f'%(0) + '\n'
+		return retString
 	if f(a)*f(b) > 0:
-		print ('error')
+		retString = 'Não há raiz no intervalo [' + str(a) + ', ' + str(b) + '].\n'
+		return retString
+
+	retString = 'k\ta\tb\txk\tek\n'
 
 	k = 0
 	err = math.inf
+
 	x = (a+b)/2
 	if ((a == -1) & (b == 0)):
 		xbarra = -1/6
 	elif ((a == 0) & (b == 1)):
 		xbarra = 5/7
-		
-	#-pow((2/(3*(9 + pow(93,(1/2))))),(1/3)) + pow((1/2*(9 + pow(93,(1/2)))),(1/3))/pow(3,(2/3))
-
-	print('k', ' \ta', ' \tb', ' \txk', ' \tek')
 
 	while ((err > tol) & (k < maxiter)):
-		print(k, '\t', a, '\t', b, '\t', x, '\t', abs(x-xbarra))
+		retString += str(k) + '\t' + '%.10f'%(a) + '\t' + '%.10f'%(b) + '\t' + '%.10f'%(x) + '\t' + '%.10f'%(abs(x-xbarra)) + '\n'
 		if f(a)*f(x) < 0:
 			b = x
 		else:
@@ -33,15 +36,29 @@ def bisection(a, b, tol, maxiter):
 		x = (a+b)/2
 		err = abs(x-x0)/max(1,x)
 		k += 1
-	return x
+
+	return retString
 
 def main():
+	#Config do arquivo
+	PATH = './saida_bisseccao.xls'
+	file = open(PATH, 'w+')
 
 	tol = pow(10,-6)
-	maxiter = 10000000	
-	bisection(-1, 0, tol, maxiter)
-	print('\n\n\n')
-	bisection(0, 1, tol, maxiter)
+	maxiter = 10000000
+
+	#Executa primeiro intervalo
+	saida = bisection(-1, 0, tol, maxiter)
+	print(saida, '\n')
+	file.write(saida)
+	file.write('\n')
+	
+	#Executa segundo intervalo
+	saida = bisection(0, 1, tol, maxiter)
+	print(saida)
+	file.write(saida)
+
+	file.close()
 
 if __name__ == "__main__":
 	main()
--- secante.py
+++ secante.py
@@ -3,12 +3,9 @@ import math
 def f(x):
 	return 42*pow(x,4) - 23*pow(x,3) + 163*pow(x,2) - 92*x - 20
 
-def fPrime(x):
-	return 168*pow(x,3) - 69*pow(x,2) + 326*x -92
-
-def newton(a, b, tol, maxiter):
+def secante(a, b, tol, maxiter):
 	
-	retString = 'k\txk\tf(xk)\tf\'(xk)\tek\n'
+	retString = 'k\txk\tf(xk)\tek\n'
 
 	if f(a) == 0:
 		retString += '0\t' + '%.10f'%(a) + '\t' + '%.10f'%(0) + '\t' + '%.10f'%(0) + '\n'
@@ -17,7 +14,7 @@ def newton(a, b, tol, maxiter):
 		retString += '0\t' + '%.10f'%(b) + '\t' + '%.10f'%(0) + '\t' + '%.10f'%(0) + '\n'
 		return retString
 	if f(a)*f(b) > 0:
-		retString = 'Não há raiz no intervalo [' + str(a) + ', ' + str(b) + '].\n'
+		retString = 'Erro: Não há raiz no intervalo [' + str(a) + ', ' + str(b) + '].\n'
 		return retString
 
 	k = 0
@@ -29,45 +26,48 @@ def newton(a, b, tol, maxiter):
 		xbarra = 5/7
 
 	x0 = a
+	x1 = b
 	x = a
 	
 	while ((err > tol) & (k < maxiter)):
-		retString += str(k) +'\t' + '%.10f'%(x0) + '\t' + '%.10f'%(f(x0)) + '\t' + '%.10f'%(fPrime(x0)) + '\t' + '%.10f'%(abs(x-xbarra)) + '\n'
-		if((x < a) | (x > b) | (fPrime(x0) == 0)):
+		retString += str(k) + '\t' + '%.10f'%(x0) + '\t' + '%.10f'%(f(x0)) + '\t' + '%.10f'%(abs(x-xbarra)) + '\n'
+		if((x < a) | (x > b) | (f(x1)-f(x0) == 0)):
 			retString += 'Erro: Não foi possivel executar o método da secante, pois no intervalo [' + str(a) + ', ' + str(b) + '] f\'(x) possui zero.\n'
-			return retString
-		else:
-			x = x0 - f(x0)/fPrime(x0)
+			#return retString
+		#else:
+		x = (f(x1)*x0-f(x0)*x1)/(f(x1)-f(x0))
 		err = abs(x-x0)/max(1,x)
-		x0 = x
+		x0 = x1
+		x1 = x
+		
 		k += 1
 
 	return retString
 
 def main():
 	#Config do arquivo
-	PATH = './saida_newton.xls'
+	PATH = './saida_secante.xls'
 	file = open(PATH, 'w+')
 
 	tol = pow(10,-6)
-	maxiter = 10000000	
+	maxiter = 10000000
 
 	#Executa primeiro intervalo
-	saida = newton(-1, 0, tol, maxiter)
-	print(saida,'\n')
+	saida = secante(-1, 0, tol, maxiter)
+	print(saida, '\n')
 	file.write(saida)
 	file.write('\n')
 
 	#Executa segundo intervalo
-	saida = newton(0, 1, tol, maxiter) #Erro
-	print(saida, '\n')
-	file.write(saida)
-	file.write('\n')
-	
-	#Executa segundo intervalo corrigindo o intervalor para [0.5, 1]
-	saida = newton(0.5, 1, tol, maxiter)
+	saida = secante(0, 1, tol, maxiter) #Erro
 	print(saida)
 	file.write(saida)
+
+	#Executa segundo intervalo corrigindo o intervalor para [0.5, 1]
+	saida = secante(0.5, 1, tol, maxiter)
+	print(saida)
+	file.write(saida)
+
 
 	file.close()
 	
